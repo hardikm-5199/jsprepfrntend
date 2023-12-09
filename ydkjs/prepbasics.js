f43b3a3f98d166579d14411a7ccfb32061172fac
hardikm-5199 <98281735+hardikm-5199@users.noreply.github.com>
2023-12-09 19:23:10 +0000

    ydkjs update

---
+++ ydkjs/prepbasics.js
@@ -0,0 +1,6 @@
+/*
+-> javascript is interpreted language but it is not entirely true. The javascript engine actually compiles 
+program on the fly and then immediately runs the compiled code
+-> console.log(b).. log(b) is function call and console is object reference where log() function is located.
+->object property access -- objects are values that hold other values at specific named locations 
+called properties. */
